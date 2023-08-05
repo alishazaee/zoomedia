85dd731582d26f51f52dc0ea0bbe805cc52451df
Ali Shazaei <alishazaee@gmail.com>
2023-08-05 15:42:04 +0330

    added username

--- zoomedia/users/models.py
+++ zoomedia/users/models.py
@@ -8,11 +8,11 @@ from django.contrib.auth.models import PermissionsMixin
 
 
 class BaseUserManager(BUM):
-    def create_user(self, email, is_active=True, is_admin=False, password=None):
+    def create_user(self, email,username , is_active=True , is_admin=False, password=None):
         if not email:
             raise ValueError("Users must have an email address")
 
-        user = self.model(email=self.normalize_email(email.lower()), is_active=is_active, is_admin=is_admin)
+        user = self.model(email=self.normalize_email(email.lower()), username=username, is_active=is_active, is_admin=is_admin)
 
         if password is not None:
             user.set_password(password)
@@ -24,9 +24,10 @@ class BaseUserManager(BUM):
 
         return user
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, email, username ,  password=None):
         user = self.create_user(
             email=email,
+            username=username,
             is_active=True,
             is_admin=True,
             password=password,
@@ -43,13 +44,14 @@ class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(verbose_name = "email address",
                               unique=True)
 
+    username = models.CharField(max_length=255 , unique=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
     
     objects = BaseUserManager()
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = "username"
 
     def __str__(self):
         return self.email
