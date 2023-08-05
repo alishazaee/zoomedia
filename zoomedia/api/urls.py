400567dbb875b0d528482ffa696fd3b630a76952
Ali Shazaei <alishazaee@gmail.com>
2023-08-05 14:54:20 +0330

    added urls into zoomedia.api

--- zoomedia/api/urls.py
+++ zoomedia/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 
 urlpatterns = [
      path('zoomedia/users/', include(('zoomedia.users.urls', 'users')) , name='users'),
-     path('zoomedia/auth/', include(('zoomedia.authentication.urls' , 'auth')) , name='auth')
+     path('zoomedia/auth/', include(('zoomedia.authentication.urls' , 'auth')) , name='auth'),
+     path('zoomedia/blog/', include(('zoomedia.blog.urls' , 'blog')) , name='blog')
+
 ]
