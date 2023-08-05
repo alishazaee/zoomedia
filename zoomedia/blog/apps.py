cd0b69411593c274053a6f9371b7ed9a1ab6ec24
Ali Shazaei <alishazaee@gmail.com>
2023-08-05 15:37:50 +0330

    added signals and profile

--- zoomedia/blog/apps.py
+++ zoomedia/blog/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'zoomediablog'
+
+    def ready(self) -> None:
+        import blog.signals.delete_follower
--- zoomedia/blog/models.py
+++ zoomedia/blog/models.py
@@ -1,2 +1,19 @@
 from django.db import models
+from zoomedia.common.models import BaseModel
+from django.conf import settings
+from django.core.exceptions import ValidationError
+class Profile(BaseModel):
+    user= models.ForeignKey(settings.AUTH_USER, on_delete=models.CASCADE ,related_name='user')
+    following_count= models.PositiveBigIntegerField(default=0)
+    follower_count = models.PositiveBigIntegerField(default=0)
 
+class Follow(BaseModel):
+    follower=models.ForeignKey(settings.AUTH_USER, on_delete=models.CASCADE ,related_name='follower')
+    following=models.ForeignKey(settings.AUTH_USER, on_delete=models.CASCADE ,related_name='following')
+    class Meta:
+        unique_together = ('follower', 'following')
+    
+    def clean(self):
+        if self.src_user == self.target_user:
+            raise ValidationError("You can not follow yourself")
+    
